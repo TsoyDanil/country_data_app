199a865821f10addf4e4857d94322903c71f51a9
danil tsoy <abdullaevdanil2002@gmail.com>
2022-12-02 19:11:35 +0600

    #1 added preloader and it's styles

---
+++ src/components/UI/Preloader/Preloader.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Preloader.css'
+
+const Preloader = () => {
+    return( 
+        <div className="Preloader">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Preloader
